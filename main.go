e5fb6018260425b7b98ab2a320318c32d4d4d2a9
matsuyoshi30 <sfbgwm30@gmail.com>
2022-04-14 01:23:44 +0900

    Parse pkg.go.dev html instead of using api.godoc.org

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -8,24 +9,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/fatih/semgroup"
+	"golang.org/x/net/html"
 	"golang.org/x/tools/go/packages"
 )
 
 var stdlibs []string
 
-const apiURL = "https://api.godoc.org/importers/"
-
-type APIResp struct {
-	Results []Result `json:"results"`
-}
-
-type Result struct {
-	Path     string `json:"path"`
-	Synopsis string `json:"synopsis"`
-}
+const pkgURL = "https://pkg.go.dev/"
 
 func init() {
 	pkgs, err := packages.Load(nil, "std")
@@ -38,27 +33,34 @@ func init() {
 	}
 }
 
+type Result struct {
+	LibPath string `json:"libPath"`
+	Num     int    `json:"num"`
+}
+
 func main() {
-	resp, err := http.Get(apiURL)
+	resp, err := http.Get(pkgURL)
 	if err != nil {
 		fmt.Println(err)
 		return
 	} else if resp.StatusCode != http.StatusOK {
-		fmt.Println("api.godoc.org returns not 200")
+		fmt.Println("pkg.go.dev returns not 200")
 		return
 	}
 
-	results := make(map[string]int)
+	results := make([]Result, 0)
 
-	sg := semgroup.NewGroup(context.Background(), 20)
+	sg := semgroup.NewGroup(context.Background(), 1)
 	for _, stdlib := range removeInternalPkg(stdlibs) {
 		stdlib := stdlib
 		sg.Go(func() error {
 			log.Printf("[INFO] start request about package %s\n", stdlib)
-			resp, err := http.Get(apiURL + stdlib)
+			resp, err := http.Get(pkgURL + stdlib)
 			log.Printf("[INFO] finish request about package %s\n", stdlib)
 			if err != nil {
 				return err
+			} else if resp.StatusCode != http.StatusOK {
+				return nil
 			}
 			defer resp.Body.Close()
 
@@ -68,21 +70,35 @@ func main() {
 				return err
 			}
 
-			var apiResp APIResp
-			err = json.Unmarshal(body, &apiResp)
+			doc, err := html.Parse(bytes.NewReader(body))
 			if err != nil {
 				log.Printf("[ERR ] package %s: %q\n", stdlib, err)
 				return err
 			}
 
-			results[stdlib] = len(apiResp.Results)
+			ret := extractImportedBy(doc)
+			if ret == "" {
+				err := fmt.Errorf("failed to extract imported-by value about %s", stdlib)
+				log.Printf("[ERR ] package %s: %q\n", stdlib, err)
+				return err
+			}
+
+			importedBy, err := strconv.Atoi(strings.ReplaceAll(strings.TrimSpace(ret), ",", ""))
+			if err != nil {
+				log.Printf("[ERR ] package %s: %q\n", stdlib, err)
+				return err
+			}
+
+			results = append(results, Result{stdlib, importedBy})
 			return nil
 		})
 	}
 
 	if err := sg.Wait(); err != nil {
-		log.Println("something happened")
+		fmt.Println(err)
 	}
+
+	sort.Slice(results, func(i, j int) bool { return results[i].Num > results[j].Num })
 
 	j, err := json.Marshal(results)
 	if err != nil {
@@ -111,5 +127,35 @@ func removeInternalPkg(libs []string) []string {
 			ret = append(ret, lib)
 		}
 	}
+
+	return ret
+}
+
+// extractImportedBy returns `imported-by` value.
+// For example, return 19,638 if given below
+//
+//   <span class="go-Main-headerDetailItem" data-test-id="UnitHeader-importedby">
+//     <a href="/runtime/debug?tab=importedby" aria-label="Go to Imported By"
+//         data-gtmc="header link">
+//        <span class="go-textSubtle">Imported by: </span>19,638
+//     </a>
+//   </span>
+func extractImportedBy(n *html.Node) string {
+	if n.Type == html.ElementNode && n.Data == "span" {
+		for _, attr := range n.Attr {
+			if attr.Key == "data-test-id" && attr.Val == "UnitHeader-importedby" {
+				return n.FirstChild.NextSibling.FirstChild.NextSibling.NextSibling.Data
+			}
+		}
+	}
+
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if ret2 := extractImportedBy(c); ret2 != "" {
+			ret = ret2
+			break
+		}
+	}
+
 	return ret
 }
